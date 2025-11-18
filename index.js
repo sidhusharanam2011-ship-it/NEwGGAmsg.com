be87e5fbca4ec090663501077d40cd98126ce8a3
sidhusharanam2011-ship-it <sidhusharanam2011@gmail.com>
2025-11-18 17:16:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+window.alert("this is an alert")
+window.alert("i'm a new learner of web game dev")
+document.getElementById("H1Id").textContent = "Hi this is written in js";
+document.getElementById("pId").textContent = "Hi this is A para edited with js";
+let x;
+let y;
+x = "Hello World";
+y = 200;
+document.getElementById("p2").textContent = typeof y;
